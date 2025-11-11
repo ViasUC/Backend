3fb04b346cac02e742469dea6a0f3c189dd141e4
lucasburgos <lucasburgosc@gmail.com>
2025-11-11 19:16:27 -0300

    Ultimos cambios de perfil

---
+++ src/main/java/com/vias/uc/backend/repository/PortafolioRepository.java
@@ -0,0 +1,11 @@
+package com.vias.uc.backend.repository;
+
+import com.vias.uc.backend.model.Portafolio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PortafolioRepository extends JpaRepository<Portafolio, Integer> {
+
+    Optional<Portafolio> findByIdUsuario(Integer idUsuario);
+}
